8270e7dd9392441c6376c72886ba173624cca63d
mishidemudong <liang.dongkai@163.com>
2021-05-14 15:00:05 +0800

    add resort module

---
+++ Rec_Server/resort_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 14:52:23 2021
+
+@author: liang
+"""
+
+def resort_func(result):
+    
+    
+    
+    
+    
+    return result
