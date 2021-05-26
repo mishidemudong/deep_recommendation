cc7545a140f0f7cd2b88d6f3386d66366e8e242c
mishidemudong <liang.dongkai@163.com>
2021-05-26 16:54:36 +0800

    add fea config 333

--- Rec_Server/utils/fea_config.py
+++ Rec_Server/utils/fea_config.py
@@ -6,6 +6,12 @@ Created on Wed May 26 15:28:29 2021
 @author: liang
 """
 
+#import pandas as pd
+#df = pd.read_csv('./interaction_fea.csv')
+#iteraction_fea = {}
+#for key, value in zip(df['字段名称'], df['英文标识']):
+#    iteraction_fea[key] = value
+
 item_fea = {'物品ID': 'item_id',
              '物品类别': 'item_type',
              '24 小时趋势': '24Htrend',
@@ -35,10 +41,32 @@ item_fea = {'物品ID': 'item_id',
         }
 
 user_fea = {
-        111
+         '测试用户': 'if_test',
+         '用户长id': 'user_id?',
+         '用户短id': '?',
+         'KYC等级': 'kyc_level',
+         'KYC国家': 'kyc_country',
+         '注册时间': 'register_time',
+         '30天持仓均值': '30days_avg_holdshares',
+         '30天手续费收入': '30days_fee',
+         '最后一次交易时间间隔': 'interval_of_last_trade',
+         '60天内交易次数': 'tradetimes_in_60days',
+         '60天内交易金额': 'trademoney_in_60days'
         }
 
 iteraction_fea = {
-        
+        '用户ID': 'user_id',
+         '物品ID': 'item_id',
+         '交互类型': 'interaction_type',
+         '交互时间': 'interaction_time',
+         '物品所在顺序': 'item_order_index',
+         '历史点击次数': 'click_times',
+         '历史购买次数': 'purchase_times',
+         '历史交互记录': 'history_interaction_list',
+         '上次点击时间': 'lasttime_click_date',
+         '上次购买时间': 'lasttime_purchase_date',
+         '上次卖出时间': 'lasttime_sold_date',
+         '上次评论时间': 'lasttime_comment_date',
+         '评论内容': 'comment'
         }
 
